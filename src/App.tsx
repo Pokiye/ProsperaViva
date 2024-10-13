ba501250c45cfc585fe6d7ff8fc11f041a3418a7
Pokiye <pokecraftll48@gmail.com>
2024-10-13 00:23:53 -0400

    Added TS Configs

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+// src/App.tsx
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+    return <h1>This is my new React app!</h1>;
+};
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,31 @@
-import path from "path"
+import path from "path";
 const __dirname = path.resolve();
 
 export default {
-  entry: './src/index.jsx',
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', "@babel/preset-react"]
-          }
-        },
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    compress: true,
-    port: 9000
-  }
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: 'ts-loader',
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        compress: true,
+        port: 9000
+    }
 };
